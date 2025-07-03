fddbe5d8eab6829401d78d6187723d2a10fa2739
mahmud-reza-rafsun <rafsun16.it@gmail.com>
2025-07-03 23:45:33 +0600

    fix mobile device responsive

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // jobs collection
     const jobsCollections = client.db('soloDB').collection('jobs');
+    const bidsCollections = client.db('soloDB').collection('bids');
 
     // save all jobsData in db
     app.post('/add-job', async (req, res) => {
       const jobData = req.body;
       const result = await jobsCollections.insertOne(jobData);
-      console.log(result);
       res.send(result);
     })
 
@@ -79,6 +79,66 @@ async function run() {
       const result = await jobsCollections.updateOne(filter, updatedDoc, option);
       res.send(result);
     })
+    // save all bids in db
+    app.post('/add-bid', async (req, res) => {
+      const bidsData = req.body;
+      // if a user placed a bid already in this job
+      console.log(bidsData);
+      const query = { email: bidsData.email, jobId: bidsData.jobId };
+      const alreadyExist = await bidsCollections.findOne(query)
+      if(alreadyExist) return res.status(400).send('You are already placed a bid for this job!')
+      console.log('if already exitis ==>', alreadyExist);
+      console.log(query);
+      // save data in bids collection
+      const result = await bidsCollections.insertOne(bidsData);
+      // increse bid count in job collection 
+      const filter = { _id: new ObjectId(bidsData.jobId) };
+      const update = {
+        $inc: { bidCount: 1, }
+      }
+      const updateBidCount = await jobsCollections.updateOne(filter, update)
+      res.send(result);
+    })
+    // get bids data for a specific user
+    app.get('/bids/:email', async (req, res) => {
+      const isbuyer = req.query.buyer;
+      const email = req.params.email;
+      const query = {};
+      if (isbuyer) {
+        query.buyer = email
+      }
+      else{
+        query.email = email
+      }
+      const result = await bidsCollections.find(query).toArray();
+      res.send(result);
+    })
+    app.patch('/bid-status-update/:id', async (req, res) => {
+      const id = req.params.id;
+      const {status} = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const update = {
+        $set: {status},
+      }
+      const result = await bidsCollections.updateOne(filter, update);
+      res.send(result);
+    })
+    // get all jobs
+    app.get('/all-jobs', async(req, res) => {
+      const filter = req.query.filter;
+      const search = req.query.search;
+      const sort = req.query.sort;
+      let options = {}
+      if(sort) options = {sort: {deadline: sort === 'asc' ? 1 : -1}}
+      let query = {title: {
+        $regex: search,
+        $options: 'i',
+      }}
+      if(filter) query.category = filter;
+      const result = await jobsCollections.find(query, options).toArray();
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -92,7 +152,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello from Solo Sphere Server....')
+  res.send('Hello from Solo Fibu Server....')
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
